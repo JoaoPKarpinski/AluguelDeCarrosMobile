d7121898143905f5bff359dd684f362fff02cd45
diogomene <diogodelimamenezes15@gmail.com>
2024-04-09 16:27:23 -0400

    First commit

---
+++ app/src/main/java/com/example/digiwagen/HomePageFragment.java
@@ -0,0 +1,44 @@
+package com.example.digiwagen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.digiwagen.databinding.FragmentHomepageBinding;
+
+public class HomePageFragment extends Fragment {
+
+    private FragmentHomepageBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentHomepageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(v ->
+                NavHostFragment.findNavController(HomePageFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
