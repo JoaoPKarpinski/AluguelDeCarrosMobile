43e3c8317a66c3d8f94a0fd1d5797832aea23691
diogomene <diogodelimamenezes15@gmail.com>
2024-04-09 21:10:36 -0400

    Adding elements and configuring param loading on car detail

--- app/src/main/java/com/example/digiwagen/CarDetailFragment.java
+++ app/src/main/java/com/example/digiwagen/CarDetailFragment.java
@@ -2,11 +2,15 @@ package com.example.digiwagen;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +21,10 @@ public class CarDetailFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM1 = "carro_escolhido";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CarListItem carroParam;
 
     public CarDetailFragment() {
         // Required empty public constructor
@@ -33,15 +35,13 @@ public class CarDetailFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment CarDetailFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CarDetailFragment newInstance(String param1, String param2) {
+    public static CarDetailFragment newInstance(String param1) {
         CarDetailFragment fragment = new CarDetailFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,15 +50,25 @@ public class CarDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            carroParam = (CarListItem) getArguments().get(ARG_PARAM1);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_car_detail, container, false);
+        View view =  inflater.inflate(R.layout.fragment_car_detail, container, false);
+
+        ImageView imagemCarro = (ImageView) view.findViewById(R.id.carDetailImageView);
+        TextView nomeCarro = (TextView) view.findViewById(R.id.carDetailNameTextView);
+
+        imagemCarro.setImageResource(carroParam.getImage());
+        nomeCarro.setText(carroParam.getName());
+
+
+        return view;
     }
+
 }
